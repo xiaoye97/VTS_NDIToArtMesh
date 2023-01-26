c102ee67521925a2990db108e00bfc9b5e9ad33c
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2023-01-26 20:24:04 +0800

    支持多NDI投到一个模型的多个ArtMesh上

--- VTS_NDIToArtMesh/NDIToArtMesh.cs
+++ VTS_NDIToArtMesh/NDIToArtMesh.cs
@@ -13,7 +13,7 @@ namespace VTS_NDIToArtMesh
     {
         public const string GUID = "me.xiaoye97.plugin.VTubeStudio.NDIToArtMesh";
         public const string PluginName = "NDIToArtMesh";
-        public const string VERSION = "1.1.0";
+        public const string VERSION = "1.2.0";
 
         private void Start()
         {
@@ -23,53 +23,59 @@ namespace VTS_NDIToArtMesh
         [HarmonyPostfix, HarmonyPatch(typeof(VTubeStudioModel), "Start")]
         public static void VTubeStudioModel_Start_Patch(VTubeStudioModel __instance)
         {
-            string path = __instance.ModelJSON.FilePath.Replace(".vtube.json", ".NDIToArtMesh.json");
-            FileInfo file = new FileInfo(path);
-            // 如果配置文件存在则尝试读取
-            if (file.Exists)
+            FileInfo modelJSonFile = new FileInfo(__instance.ModelJSON.FilePath);
+            var files = modelJSonFile.Directory.GetFiles("*.NDIToArtMesh.json");
+            foreach (var file in files)
             {
-                string json = File.ReadAllText(file.FullName);
-                try
+                if (file.Name.StartsWith(__instance.ModelJSON.Name))
                 {
-                    var config = JsonConvert.DeserializeObject<NDIToArtMeshConfig>(json);
-                    if (config.ArtMeshNames == null || config.ArtMeshNames.Count == 0)
+                    ReadConfigFile(__instance, file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 读取配置文件
+        /// </summary>
+        public static void ReadConfigFile(VTubeStudioModel model, FileInfo file)
+        {
+            string json = File.ReadAllText(file.FullName);
+            try
+            {
+                var config = JsonConvert.DeserializeObject<NDIToArtMeshConfig>(json);
+                if (config.ArtMeshNames == null || config.ArtMeshNames.Count == 0)
+                {
+                    Debug.LogWarning($"NDIToArtMesh 模型:{model.ModelJSON.Name} 的NDIToArtMesh配置文件中没有目标ArtMesh，不进行NDI接收");
+                    return;
+                }
+                List<Renderer> renderers = new List<Renderer>();
+                foreach (var d in model.Live2DModel.Drawables)
+                {
+                    // ID相同，找到目标
+                    if (config.ArtMeshNames.Contains(d.Id))
                     {
-                        Debug.LogWarning($"NDIToArtMesh 模型:{__instance.ModelJSON.Name} 的NDIToArtMesh配置文件中没有目标ArtMesh，不进行NDI接收");
-                        return;
-                    }
-                    List<Renderer> renderers = new List<Renderer>();
-                    foreach (var d in __instance.Live2DModel.Drawables)
-                    {
-                        // ID相同，找到目标
-                        if (config.ArtMeshNames.Contains(d.Id))
-                        {
-                            var renderer = d.GetComponent<Renderer>();
-                            renderers.Add(renderer);
-                        }
-                    }
-                    if (renderers.Count > 0)
-                    {
-                        var ndi = __instance.GetComponent<XYNdiReceiver>();
-                        if (ndi == null)
-                        {
-                            ndi = __instance.gameObject.AddComponent<XYNdiReceiver>();
-                        }
-                        ndi.ndiName = config.NDIName;
-                        ndi.HFlip = config.HorizontalFlip;
-                        ndi.VFlip = config.VerticalFlip;
-                        ndi.targetMaterialProperty = "_MainTex";
-                        ndi.targetRenderers = renderers;
-                        Debug.Log($"NDIToArtMesh在模型:{__instance.ModelJSON.Name} 的{renderers.Count}/{config.ArtMeshNames.Count}个ArtMesh上开启了NDI接收");
-                    }
-                    else
-                    {
-                        Debug.Log($"NDIToArtMesh 模型:{__instance.ModelJSON.Name} 上没有目标的ArtMesh，不进行NDI接收");
+                        var renderer = d.GetComponent<Renderer>();
+                        renderers.Add(renderer);
                     }
                 }
-                catch (Exception ex)
+                if (renderers.Count > 0)
                 {
-                    Debug.LogError($"NDIToArtMesh解析配置文件异常 {ex}");
+                    var ndi = model.gameObject.AddComponent<XYNdiReceiver>();
+                    ndi.ndiName = config.NDIName;
+                    ndi.HFlip = config.HorizontalFlip;
+                    ndi.VFlip = config.VerticalFlip;
+                    ndi.targetMaterialProperty = "_MainTex";
+                    ndi.targetRenderers = renderers;
+                    Debug.Log($"NDIToArtMesh在模型:{model.ModelJSON.Name} 的{renderers.Count}/{config.ArtMeshNames.Count}个ArtMesh上开启了NDI接收");
                 }
+                else
+                {
+                    Debug.Log($"NDIToArtMesh 模型:{model.ModelJSON.Name} 上没有目标的ArtMesh，不进行NDI接收");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"NDIToArtMesh解析配置文件异常 {ex}");
             }
         }
     }
